b927f65d8c4c04c8a7e39c3b8c6e42116d1822d5
Satoshi Deguchi <deguchik@gmail.com>
2020-02-05 12:08:33 +0900

    update

--- src/yomitai.js
+++ src/yomitai.js
@@ -13,6 +13,54 @@ import {
     check
 } from './api_calil.js'
 
+
+function AverageColorByImage(src) {
+    const img = new Image();
+    img.src = src;
+    img.crossOrigin = "anonymous";
+    const canvas = document.createElement("canvas")
+    const ctx = canvas.getContext('2d');
+    return new Promise((resolve, reject) => {
+      img.onload = () => {
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, img.width, img.height)
+        let rgba = [0,0,0,0]     
+      
+        imageData.data.forEach((v,i) => {
+          rgba[i % 4] = rgba[i % 4] + v   
+        })
+        const r = rgba[0] / (img.width * img.height)
+        const g = rgba[1] / (img.width * img.height)
+        const b = rgba[2] / (img.width * img.height)
+        console.log([r,g,b])
+        resolve([r,g,b])
+      };
+      img.onerror = (e) => reject(e)
+    });
+}
+
+function rgbToHsl(rgb){
+    const r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255;
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+  
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    return [h, s, l];
+}
+
 export default class Yomitai {
     constructor(props) {
         this.props = props;
@@ -266,6 +314,13 @@ export default class Yomitai {
      * @param systemids
      */
     check(isbns, systemids) {
+
+        // 書影の平均色を出す
+        isbns.map((isbn) => {
+            AverageColorByImage('https://calil.jp/cover/' + isbn).then((rgb) => {
+                console.log(rgb)
+            })
+        })
 
         // 蔵書状況をリセット
         this.props.books.map((book) => {
