8bcda983bc054f623db5fc5c59301d3f88c7a60a
Satoshi Deguchi <deguchik@gmail.com>
2020-02-12 10:00:42 +0900

    remove lightness

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import Yomitai from './yomitai';
-import Lightness from './lightness';
 
 window.initYomitai = (appkey, systemids, isbns, callback) => {
     return new Yomitai({
         appkey: appkey,
         isbns: isbns,
         systemids: systemids,
-        callback: callback
-    })
-}
-
-window.initLightness = (isbns, callback) => {
-    return new Lightness({
-        isbns: isbns,
         callback: callback
     })
 }
